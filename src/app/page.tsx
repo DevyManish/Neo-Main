d442620e3e567f0dbd7f3d2d3b0475dd3352f834
DevyManish <manishguptacpn@gmail.com>
2025-08-23 17:38:09 +0530

    (ui): add light rays

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,21 @@
 import { Hero } from "@/components/hero";
+import LightRays from "@/components/ui/lightrays";
 
 export default function Home() {
   return (
     <>
+      <LightRays
+        raysOrigin="top-center"
+        raysColor="#00ffff"
+        raysSpeed={1.5}
+        lightSpread={0.8}
+        rayLength={1.2}
+        followMouse={true}
+        mouseInfluence={0.1}
+        noiseAmount={0.1}
+        distortion={0.05}
+        className="custom-rays"
+      />
       <Hero />
     </>
   );
